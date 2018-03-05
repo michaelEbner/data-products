af0e87a436d53c160536cdf5db628d087940e241
mopedRudl <michaelebner9@gmail.com>
2018-03-05 22:04:28 +1100

    update

---
+++ courts.R
@@ -0,0 +1,17 @@
+require(rjson)
+
+
+setwd("/Users/mickey/Documents/GitHub/data-products")
+
+if (!file.exists("courts.csv")){
+  fileURL <- "https://www.data.act.gov.au/api/views/igti-4f4a/rows.csv?accessType=DOWNLOAD"
+  download.file(fileURL, "courts.csv", method="curl")
+} 
+
+
+courts <- read.csv("courts.csv") %>% select(ID, TYPE, DIVISION,LATITUDE, LONGITUDE)
+
+courts %>%
+  leaflet() %>%
+  addTiles() %>%
+  addMarkers(clusterOptions = markerClusterOptions())
