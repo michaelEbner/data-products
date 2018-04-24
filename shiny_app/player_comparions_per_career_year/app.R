40acc0792c0a34c44646be56a3badc8cbce8f9c7
Michael Ebner <mebner@atlassian.com>
2018-04-24 20:47:42 +1000

    fixed

--- shiny_app/player_comparions_per_career_year/app.R
+++ shiny_app/player_comparions_per_career_year/app.R
@@ -6,14 +6,13 @@ library(htmltab)
 library(stringr)
 library(shiny)
 
-setwd("/Users/mebner/Documents/for_me/R_coursera/GitHub/shiny_app/player_comparions_per_career_year/")
+setwd("/Users/mebner/Documents/for_me/R_coursera/GitHub/data-products/shiny_app/player_comparions_per_career_year/")
 
 df_players = data.frame()
 for (i in  letters){
   
   # vector output
-  #players <- try(get_players(i))
-  players <-  htmltab(doc = paste0("https://www.basketball-reference.com/players/",i), which = 1, header = 1,rm_nodata_cols = F)
+  players <- try(get_players(i))
   df_players <- rbind(df_players,players)
 }
 
@@ -22,35 +21,81 @@ players$name <- paste0(sub("[a-zA-Z\\.\\'\\-]+ ", "", players$player)," ",sub(" 
 
 slugs <- unlist(as.list(players$slug))
 
-df = data.frame()
+player_stats = data.frame()
 for (i in  slugs){
-  
   # vector output
   initials <- paste0(substr(i,1,1),"/")
-  player_stats <- htmltab(doc = paste0("https://www.basketball-reference.com/players/",initials,i,".html"), which = 1, header = 1,rm_nodata_cols = F)
-  df_players <- rbind(df,player_stats)
+  selection <- htmltab(doc = paste0("https://www.basketball-reference.com/players/",initials,i,".html"), which = 1, header = 1,rm_nodata_cols = F)
+  selection$slug <- paste0(i)
+  player_stats <- rbind(player_stats,selection)
 }
+
+# "armstbj01"
+# "Abdul-Rauf Mahmoud"
 
 runApp(list(
   ui = fluidPage(
     selectInput("player1", "Select a player",
-              list(`A` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^A.*")))),use.names = F),
-                   `B` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^B.*")))),use.names = F))
+                list(`A` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^A.*")))),use.names = F),
+                     `B` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^B.*")))),use.names = F),
+                     `C` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^C.*")))),use.names = F),
+                     `D` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^D.*")))),use.names = F),
+                     `E` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^E.*")))),use.names = F),
+                     `F` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^F.*")))),use.names = F),
+                     `G` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^G.*")))),use.names = F),
+                     `H` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^H.*")))),use.names = F),
+                     `I` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^I.*")))),use.names = F),
+                     `J` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^J.*")))),use.names = F),
+                     `K` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^K.*")))),use.names = F),
+                     `L` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^L.*")))),use.names = F),
+                     `M` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^M.*")))),use.names = F),
+                     `N` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^N.*")))),use.names = F),
+                     `O` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^O.*")))),use.names = F),
+                     `P` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^P.*")))),use.names = F),
+                     `Q` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^Q.*")))),use.names = F),
+                     `R` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^R.*")))),use.names = F),
+                     `S` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^S.*")))),use.names = F),
+                     `T` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^T.*")))),use.names = F),
+                     `U` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^U.*")))),use.names = F),
+                     `V` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^V.*")))),use.names = F),
+                     `W` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^W.*")))),use.names = F),
+                     `X` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^X.*")))),use.names = F),
+                     `Y` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^Y.*")))),use.names = F),
+                     `Z` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^Z.*")))),use.names = F))
     ),
-    verbatimTextOutput('bar')
+    tableOutput('table1')
   ),
-server = function(input, output) {
-  df_player1 <- reactive({
-    slug <- subset(players,name == "Bryant Kobe") %>% select(slug)
-    initial <- paste0(substr(as.character(slug[1,1]), 1, 1),"/")
-    df <- htmltab(doc = paste0("https://www.basketball-reference.com/players/",initial,as.character(slug[1,1]),".html"), which = 1, header = 1,rm_nodata_cols = F)
-    return(df)
+  server = function(input, output) {
+    df_player1 <- reactive({
+      selection <- players %>% subset(name == input$player1) %>% select(slug)
+      initial <- paste0(substr(as.character(selection), 1, 1),"/")
+      df <- htmltab(doc = paste0("https://www.basketball-reference.com/players/",initial,as.character(selection),".html"), which = 1, header = 1,rm_nodata_cols = F)
+      df <- df %>% mutate()
+      return(df)
     })
-  
-  output$table1 <- renderTable(df_player1())
-                           
+    
+    output$table1 <- renderTable(df_player1())
+    
   }
 ))
 
 
-
+runApp(list(
+  ui = fluidPage(
+    selectInput("player1", "Select a player",
+                list(`A` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^A.*")))),use.names = F),
+                     `C` = unlist(as.list(players %>% select(name) %>% filter(!is.na(str_match(as.character(players$name),"^C.*")))),use.names = F))
+    ),
+    tableOutput('table1')
+  ),
+  server = function(input, output) {
+    df_player1 <- reactive({
+      selection <- players %>% subset(name == input$player1) %>% select(slug)
+      df <- player_stats %>% subset(slug == as.character(selection)) %>% select(-slug)
+      return(df)
+    })
+    
+    output$table1 <- renderTable(df_player1())
+    
+  }
+))
